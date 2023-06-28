ebe88ace0396317f8e2ed82a5c0c01ebe98b4721
Denilah <3103936792@qq.com>
2023-06-28 08:29:27 +0800

    Update eval

--- eval/generate_code.py
+++ eval/generate_code.py
@@ -7,17 +7,22 @@ from tqdm import tqdm
 import torch
 import gradio as gr
 import transformers
+import re
 from eval.human_eval.data import write_jsonl, read_problems, stream_jsonl
 from train import ModelArguments, smart_tokenizer_and_embedding_resize, DEFAULT_PAD_TOKEN, DEFAULT_EOS_TOKEN, \
     DEFAULT_BOS_TOKEN, DEFAULT_UNK_TOKEN
 
-PROMPT_TEMPLATE = (
-    "Below is an instruction that describes a task. Write a response that appropriately completes the request. "
-    "Write code that appropriately completes the request.\n\n"
-    "### Instruction:\nCreate a Python script for this problem:\n{instruction}\n\n### Response:"
-)
+PROMPT_TEMPLATE = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+
+### Instruction:
+Create a Python script for this problem:
+{}
+
+### Response:"""
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+STOP_SEQS = ['\nclass', '\ndef', '\n#', '\nif', '\nprint']
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='$GEMINI_PRETRAIN', help="")
@@ -32,8 +37,10 @@ parser.add_argument('--num_seqs_per_iter', type=int, default=50, help='')
 parser.add_argument('--overwrite', action='store_true', help='')
 parser.add_argument('--load_in_8bit', type=bool, default=False, help='')
 parser.add_argument('--inference_dtype', type=str, default="float32", help="The dtype to use for inference.")
-parser.add_argument('--launch_gradio', type=bool, default=False, help="Whether to use user input for prompting or a fixed eval list.")
+parser.add_argument('--launch_gradio', type=bool, default=False,
+                    help="Whether to use user input for prompting or a fixed eval list.")
 args = parser.parse_args()
+
 
 def initialize():
     global model, tokenizer
@@ -71,8 +78,25 @@ def initialize():
     )
 
 
+def extract_text(prompt, remove_lines=True):
+    token = '\"\"\"'
+    start = token
+    end = '>>>'
+
+    start_idx = prompt.find(start) + len(start)
+    end_idx = prompt.find(end)
+
+    output = prompt[start_idx: end_idx]
+    if remove_lines:
+        output = output.replace('\n', ' ')
+    output = re.sub(r"\s+", " ", output).strip()
+
+    return output
+
+
 def generate_prompt(instruction, input=None):
-    return PROMPT_TEMPLATE.format(instruction=instruction)
+    return PROMPT_TEMPLATE.format(extract_text(instruction))
+
 
 def evals():
     problems = read_problems()
@@ -83,43 +107,60 @@ def evals():
     print("Number of samples: {}".format(num_samples))
     print(f"Loaded {args.model}.")
 
-    generation_config = GenerationConfig(
-        temperature=args.temperature,
-        top_p=0.95,
-    )
     for i in tqdm(range(num_samples), ncols=0, total=num_samples):
         output_file = args.output_path + '/{}.jsonl'.format(args.start_index + i)
-        if args.start_index + i == 21 or args.start_index + i == 84:
-            continue
         if os.path.exists(output_file) and not args.overwrite:
             print(f'Skip {output_file} as it already exists')
             continue
 
         completion_seqs = []
 
-        # loops = int(args.N / args.num_seqs_per_iter)
-        instruction = prompts[i]
-        inputs = tokenizer(generate_prompt(
-            instruction, None), return_tensors="pt")
-        for _ in tqdm(range(args.N), total=args.N, leave=False, ncols=0):
-            completion_seq = get_completion(generation_config, args.max_len, inputs)
-            completion_seqs.append(
-                {'task_id': task_ids[i],
-                 'completion': completion_seq,
-                 }
-            )
+        prompt = prompts[i].replace('    ', '\t')
+        prompt_batch = [PROMPT_TEMPLATE.format(extract_text(prompt))]
 
-        print("Saving results to {}".format(output_file))
-        write_jsonl(output_file, completion_seqs)
+        encoding = tokenizer(prompt_batch, return_tensors="pt", truncation=True, max_length=args.max_len).to(device)
 
-def get_completion(generation_config, max_length, inputs):
+        loops = int(args.N / args.num_seqs_per_iter)
+
+        for _ in tqdm(range(loops), total=loops, leave=False, ncols=0):
+            with torch.no_grad():
+                gen_tokens = model.generate(**encoding,
+                                            do_sample=True,
+                                            temperature=args.temperature,
+                                            max_length=args.max_len,
+                                            num_return_sequences=args.max_len,
+                                            top_p=0.95)
+                gen_tokens = gen_tokens[:, encoding['input_ids'].shape[-1]:]
+                gen_seqs = tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)
+                for seq_idx, gen_seq in enumerate(gen_seqs):
+                    completion_seq = gen_seq
+                    for stop_seq in STOP_SEQS:
+                        index = completion_seq.find(stop_seq)
+                        if index != -1:
+                            completion_seq = completion_seq[:index]
+                    completion_seq = completion_seq.replace('\t', '    ')
+                    all_code = prompt.replace('\t', '    ') + completion_seq
+
+                    completion_seqs.append(
+                        {'task_id': task_ids[i],
+                         'completion': completion_seq,
+                         'all_code': all_code
+                         }
+                    )
+
+            print("Saving results to {}".format(output_file))
+            write_jsonl(output_file, completion_seqs)
+
+
+def get_completion(generation_config, max_length, instruction):
+    inputs = tokenizer(generate_prompt(
+        instruction, None), return_tensors="pt")
     with torch.no_grad():
         outputs = model.generate(input_ids=inputs["input_ids"].to(device),
-                                 generation_config=generation_config,
-                                 do_sample=True,
-                                 max_new_tokens=max_length,
-                                 return_dict_in_generate=True,
-                                 output_scores=True)
+                                generation_config=generation_config,
+                                max_new_tokens=max_length,
+                                return_dict_in_generate=True,
+                                output_scores=True)
     input_length = 1 if model.config.is_encoder_decoder else inputs.input_ids.shape[
         1]
     generated_tokens = outputs.sequences[:, input_length:]
@@ -140,6 +181,7 @@ def gradio_inference(
         num_beams=num_beams,
     )
     return get_completion(generation_config, max_new_tokens, instruction)
+
 
 def run_interface():
     g = gr.Interface(
@@ -167,6 +209,7 @@ def run_interface():
     )
     g.queue(concurrency_count=1)
     g.launch(share=True)
+
 
 if __name__ == '__main__':
     initialize()
